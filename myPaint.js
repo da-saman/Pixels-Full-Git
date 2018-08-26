3f020c2fe67e47b8b4609cc2290a20a5a4e3a844
User 1 <darabi.s.i@gmail.com>
2018-08-26 17:06:09 +0430

    add the Circle tool by user 1

--- myPaint.js
+++ myPaint.js
@@ -132,7 +132,27 @@ function draw(pos, state, dispatch) {
     return drawPixel;
 }
 
-
+function circle(pos, state, dispatch) {
+    function drawCircle(to) {
+      let radius = Math.sqrt(Math.pow(to.x - pos.x, 2) +
+                             Math.pow(to.y - pos.y, 2));
+      let radiusC = Math.ceil(radius);
+      let drawn = [];
+      for (let dy = -radiusC; dy <= radiusC; dy++) {
+        for (let dx = -radiusC; dx <= radiusC; dx++) {
+          let dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+          if (dist > radius) continue;
+          let y = pos.y + dy, x = pos.x + dx;
+          if (y < 0 || y >= state.picture.height ||
+              x < 0 || x >= state.picture.width) continue;
+          drawn.push({x, y, color: state.color});
+        }
+      }
+      dispatch({picture: state.picture.draw(drawn)});
+    }
+    drawCircle(pos);
+    return drawCircle;
+  }
 function historyUpdateState(state, action) {
 
     return Object.assign({}, state, action);
@@ -145,7 +165,7 @@ var startState = {
     picture: Picture.empty(60, 30, "#f0f0f0"),
 };
 
-var baseTools = { draw };
+var baseTools = { draw,circle };
 
 var baseControls = [ToolSelect];
 
